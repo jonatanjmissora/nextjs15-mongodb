5949d81b799e7d4b2698cba5157fe79de5c54df8
Jonatan Missora <jonatanjmissora@gmail.com>
2025-03-06 16:39:25 -0300

    sds

--- app/_components/Header/User_Menu.tsx
+++ app/_components/Header/User_Menu.tsx
@@ -4,6 +4,7 @@ import UserSVG from "@/app/_assets/UserSVG";
 import Image from "next/image";
 import { useRef } from "react";
 import Theme_Toggler from "./Theme_Toggler";
+import UploadSVG from "@/app/_assets/UploadSVG";
 
 export default function User_Menu({ user }: { user: { username: string, image: string } }) {
 
@@ -15,7 +16,12 @@ export default function User_Menu({ user }: { user: { username: string, image: s
 
       <div className="card flex flex-col justify-end items-end gap-12 absolute top-12 -right-6 w-[15rem]">
         <div className="flex flex-col items-center justify-center">
-          <img src={"avatar.jpg"} className="size-20 rounded-full" alt={"avatar image"} />
+          <div className="relative">
+            <img src={"avatar.jpg"} className="size-20 rounded-full" alt={"avatar image"} />
+            <button className="absolute -right-4 bottom-0 bg-slate-800 rounded-full p-1">
+              <UploadSVG className="size-5" />
+            </button>
+          </div>
           <p>Hola, {user.username}</p>
         </div>
         <p className="hover">Cerrar sesión</p>
