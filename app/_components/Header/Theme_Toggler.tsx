bbd52b51639176e48c7067012b3b4888f1ac082f
Jonatan Missora <jonatanjmissora@gmail.com>
2025-03-06 15:59:34 -0300

    haciendo el user menu

--- app/_components/Header/Theme_Toggler.tsx
+++ app/_components/Header/Theme_Toggler.tsx
@@ -17,15 +17,15 @@ const Theme_Toggler = () => {
         theme === "light"
 
           ? <div
-            className='flex items-center justify-center gap-4'>
-            <span className='inline-block sm:hidden'>Tema</span>
-            <SunSVG className="size-6 hover:rotate-180 hover:text-[var(--hover-primary)] duration-500" />
+            className='flex items-center justify-center gap-4 group'>
+            <span className='group-hover:text-[var(--hover-primary)] duration-300'>Tema</span>
+            <SunSVG className="size-6 group-hover:rotate-180 group-hover:text-[var(--hover-primary)] duration-300" />
           </div>
 
           : <div
-            className='flex items-center justify-center gap-4'>
-            <span className='inline-block sm:hidden'>Tema</span>
-            <MoonSVG className="size-6 hover:rotate-235 hover:text-[var(--hover-primary)] duration-500" />
+            className='flex items-center justify-center gap-4 group'>
+            <span className='group-hover:text-[var(--hover-primary)] duration-300'>Tema</span>
+            <MoonSVG className="size-6 group-hover:rotate-235 group-hover:text-[var(--hover-primary)] duration-300" />
           </div>
       }
     </button>
--- app/_components/Header/User_Menu.tsx
+++ app/_components/Header/User_Menu.tsx
@@ -5,20 +5,23 @@ import Image from "next/image";
 import { useRef } from "react";
 import Theme_Toggler from "./Theme_Toggler";
 
-export default function User_Menu({user}:{user: {username: string, image: string}}) {
+export default function User_Menu({ user }: { user: { username: string, image: string } }) {
 
-    const detailRef = useRef<HTMLDetailsElement>(null)
+  const detailRef = useRef<HTMLDetailsElement>(null)
 
   return (
-    <details className="border border-yellow-500 relative">
-        <summary className="list-none border"><UserSVG className="size-6" /></summary>
+    <details className="relative">
+      <summary className="list-none hover"><UserSVG className="size-6" /></summary>
 
-        <div className="flex flex-col justify-end items-end gap-6 border absolute top-[2rem] right-0 w-[10rem]">
-            <p>Hola, {user.username}</p>
-            <p>Cerrar sesión</p>
-            <div className="flex items-center justify-end gap-4">Tema <Theme_Toggler /></div>
+      <div className="card flex flex-col justify-end items-end gap-12 absolute top-12 -right-6 w-[15rem]">
+        <div className="flex flex-col items-center justify-center">
+          <img src={"avatar.jpg"} className="size-20 rounded-full" alt={"avatar image"} />
+          <p>Hola, {user.username}</p>
         </div>
-        
-    </details>
+        <p className="hover">Cerrar sesión</p>
+        <Theme_Toggler />
+      </div>
+
+    </details >
   )
 }
